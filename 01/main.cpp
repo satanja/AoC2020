878490b786347349bdbc06feea1f0588b423c3b8
satanja <s.a.tanja@student.tue.nl>
2020-12-10 20:56:08 +0100

    Optimize 1

--- 01/main.cpp
+++ 01/main.cpp
@@ -3,44 +3,74 @@
 #include <string>
 #include <unordered_set>
 #include <vector>
-
+#include <bitset>
+#include <array>
 #include "stefan.h"
+
+template<size_t k>
+class array_vector
+{
+private:
+    std::array<int, k> arr;
+    int head = 0;
+public:
+    size_t size() 
+    {
+        return head;
+    }
+
+    size_t capacity()
+    {
+        return k;
+    }
+
+    int operator[](int index)
+    {
+        return arr[index];
+    }
+
+    void push_back(int val)
+    {
+        if (size() == arr.size()) continue;
+        arr[head] = val;
+        head++;
+    }
+};
 
 void aoc01(const char *arr, int len, unsigned char **out) {
     std::string in(arr, arr + len);
     std::istringstream cin(in);
     std::ostringstream cout((char*) *out);
     int x;
-    std::unordered_set<int> X;
+    std::bitset<2020> X;
     const int target = 2020;
     while (cin >> x)
     {
-        if (X.contains(2020 - x))
+        if (X[2020 - x])
         {
             cout << (2020 - x) * x << " ";
             goto part2;
         }
         else 
         {
-            X.insert(x);
+            X[x] = true;
         }
     }
 part2:
+    X.reset();
     cin.seekg(0);
-    std::vector<int> Y;
+    array_vector<2020> Y;
+
     while (cin >> x)
     {
         for (int i = 0; i < Y.size(); i++)
         {
-            for (int j = i + 1; j < Y.size(); j++)
+            if (X[2020 - x - Y[i]])
             {
-                if (x + Y[i] + Y[j] == 2020)
-                {
-                    cout << x * Y[i] * Y[j] << "\n";
-                    return;
-                }
+                cout << x * Y[i] * (2020 - x - Y[i]) << "\n";
             }
         }
         Y.push_back(x);
+        X[x] = true;
     }
 }
